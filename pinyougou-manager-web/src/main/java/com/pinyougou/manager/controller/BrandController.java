d89763f1621585a014155f758502e58769d453e6
15607521232 <15607521232@163.com>
2021-11-28 23:00:05 +0800

    test

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -4,6 +4,9 @@ package com.pinyougou.manager.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +23,27 @@ public class BrandController {
     @RequestMapping("/findAll")
     public List<TbBrand> findAll(){
         return brandService.findAll();
-        //test
+
+
+    }
+
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page, int rows){
+
+        return brandService.findPage(page,rows);
+
+    }
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbBrand brand){
+        try {
+            brandService.add(brand);
+            return new Result(true,"添加成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,"添加失败");
+        }
 
     }
 }
--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,10 +1,19 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
 public interface BrandService {
 
     public List<TbBrand> findAll();
+
+    /**
+     * 返回分页列表
+     */
+    public PageResult findPage(int pageNum,int pageSize);
+
+    //增加
+    public void add(TbBrand brand);
 }
