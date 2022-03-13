0dd7d69b6495e3de08fd414bed97409998bbe096
liguang <15607521232@163.com>
2022-03-13 21:41:15 +0800

    商品管理

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -32,20 +32,14 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService){
 	}
 	
 	//保存 
-	$scope.save=function(){				
-		var serviceObject;//服务层对象  				
-		if($scope.entity.id!=null){//如果有ID
-			serviceObject=goodsService.update( $scope.entity ); //修改  
-		}else{
-			serviceObject=goodsService.add( $scope.entity  );//增加 
-		}				
-		serviceObject.success(
+	$scope.add=function(){
+
+		goodsService.add( $scope.entity  ).success(
 			function(response){
+				alert(response.message);
 				if(response.success){
 					//重新查询 
-		        	$scope.reloadList();//重新加载
-				}else{
-					alert(response.message);
+		        	$scope.entity={};
 				}
 			}		
 		);				
