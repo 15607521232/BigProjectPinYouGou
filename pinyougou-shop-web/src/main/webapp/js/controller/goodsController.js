7949ee428f48407581a6c4c0f5ad8dcc700b1907
liguang <15607521232@163.com>
2022-03-21 00:20:51 +0800

    品牌列表

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -1,5 +1,5 @@
  //控制层 
-app.controller('goodsController' ,function($scope,$controller   ,goodsService,uploadService,itemCatService){
+app.controller('goodsController' ,function($scope,$controller   ,goodsService,uploadService,itemCatService,typeTemplateService){
 	
 	$controller('baseController',{$scope:$scope});//继承
 	
@@ -126,4 +126,15 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 
 
 	})
+	//查询品牌列表
+	$scope.$watch("entity.goods.typeTemplateId",function (newValue,oldValue){
+		typeTemplateService.findOne(newValue).success(function (response){
+			$scope.typeTemplate = response;//模板ID
+			$scope.typeTemplate.brandIds = JSON.parse($scope.typeTemplate.brandIds)//品牌列表由字符串转换为对象
+		})
+
+
+	})
+
+
 });
