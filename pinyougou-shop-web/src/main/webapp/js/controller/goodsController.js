e0313842c8c62be029a562dde24c5a021993f04f
liguang <15607521232@163.com>
2022-03-23 20:25:09 +0800

    扩展属性

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -131,6 +131,7 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
 		typeTemplateService.findOne(newValue).success(function (response){
 			$scope.typeTemplate = response;//模板ID
 			$scope.typeTemplate.brandIds = JSON.parse($scope.typeTemplate.brandIds)//品牌列表由字符串转换为对象
+			$scope.entity.goodsDesc.customAttributeItems = JSON.parse($scope.typeTemplate.customAttributeItems)//扩展属性
 		})
 
 
