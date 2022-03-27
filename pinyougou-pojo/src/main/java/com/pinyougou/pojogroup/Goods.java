01c9386084da210e52dd1c8bfbceb62ea0d7b877
liguang <15607521232@163.com>
2022-03-27 22:20:33 +0800

    后端商品保存

--- pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Goods.java
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Goods.java
@@ -2,8 +2,10 @@ package com.pinyougou.pojogroup;
 
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbGoodsDesc;
+import com.pinyougou.pojo.TbItem;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * 组合实体类
@@ -14,6 +16,16 @@ public class Goods implements Serializable {
     private TbGoods goods;//商品主表
 
     private TbGoodsDesc goodsDesc;//商品扩展表
+
+    private List<TbItem> itemList;//SKU列表
+
+    public List<TbItem> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<TbItem> itemList) {
+        this.itemList = itemList;
+    }
 
     public TbGoods getGoods() {
         return goods;
