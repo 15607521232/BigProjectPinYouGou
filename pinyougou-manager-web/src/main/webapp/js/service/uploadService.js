9b645a76819fa01cc51e25b96b015be76c477cbb
liguang <15607521232@163.com>
2022-04-04 23:29:28 +0800

    添加广告管理相关

---
+++ pinyougou-manager-web/src/main/webapp/js/service/uploadService.js
@@ -0,0 +1,17 @@
+//服务层
+app.service('uploadService',function($http){
+
+    //读取列表数据绑定到表单中
+    this.upload=function(){
+
+        var formData = new FormData();//表单数据对象
+        formData.append("imgFile",file.files[0]);
+        return $http({
+            url:"../upload.do",
+            method:"POST",
+            data:formData,
+            headers:{"Content-Type":undefined},
+            transformRequest:angular.identity //表单序列化
+        })
+    }
+});
