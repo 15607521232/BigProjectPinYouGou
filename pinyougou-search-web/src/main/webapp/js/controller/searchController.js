de04a7a294f2749d367038c3c12037f54a5c84bc
liguang <15607521232@163.com>
2022-04-27 00:10:32 +0800

    自定义分页

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,14 +1,41 @@
 app.controller('searchController',function ($scope,searchService){
 
     //定义搜索对象的结构
-    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':''};
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':'','pageNo':1,'pageSize':40};
 
     $scope.search=function (){
         searchService.search($scope.searchMap).success(
             function (response){
                 $scope.resultMap=response;
+                buildPageLabel();
+
             }
         )
+    }
+
+    //构建分页栏
+    buildPageLabel=function (){
+
+        $scope.pageLabel=[];
+        var firstPage=1;
+        var lastPage=$scope.resultMap.totalPages;//终止页码
+
+        if($scope.resultMap.totalPages>5){
+            //如果页码数大于5
+            if($scope.searchMap.pageNo<=3){
+                //显示前5页
+                lastPage=5;
+            }else if($scope.searchMap.pageNo>=$scope.resultMap.totalPages-2){
+                firstPage=$scope.resultMap.totalPages-4;
+            }else {
+                //显示当前页为中心的5页
+                firstPage=$scope.searchMap.pageNo-2;
+                lastPage=$scope.searchMap.pageNo+2;
+            }
+        }
+        for (let i = firstPage; i <= lastPage; i++) {
+            $scope.pageLabel.push(i);
+        }
     }
 
     //添加搜索项
@@ -31,6 +58,8 @@ app.controller('searchController',function ($scope,searchService){
         }
         $scope.search();//执行搜索
     }
+
+
 
 
 
